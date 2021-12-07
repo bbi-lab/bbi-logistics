a03b15294fc19384b303b709b6d5b3e619fcd03a
cooperqmarshall <qballer@uw.edu>
2021-12-07 13:52:53 -0800

    non-zero padding broke on windows using string.replace() instead of %-m

--- update_dashboards/courier.py
+++ update_dashboards/courier.py
@@ -8,6 +8,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 base_dir = Path(__file__).resolve().parent.parent.resolve()
 
+
 def main():
 	print("Connecting to Google Sheets")
 	# creates conneciton to google sheets with google api
@@ -19,32 +20,34 @@ def main():
 	# find what dates in the google sheets are missing
 	missing_dates = get_missing_dates(db)
 
+	print(missing_dates)
+
 	print('Getting KPI and exceptions data')
 	data = []
-	# loop through the missing days and check for shipping kpi and exceptions sheets
-	for day in missing_dates:
-		date = datetime.datetime.strptime(day, '%m/%d/%y')
-		try:
-			# get the data from both sheets
-			data.extend(get_courier_data(client, date))
 
-		except gspread.exceptions.SpreadsheetNotFound as e:
-			print('No sheet found for {}'.format(str(day)))
+	for day in missing_dates:
+		try:
+			data.extend(get_courier_data(client, day))
+
+		except gspread.exceptions.SpreadsheetNotFound:
+			print('No sheet found for {}'.format(day))
 
 		except TypeError as e:
-			print(f'KPI or exceptions returned length 0 for {str(day)} {e}')
+			print(f'KPI or exceptions returned length 0 for {day} {e}')
 
-	print('{: <30}{}'.format('Total import rows:',str(len(data))))
+	print('{: <30}{}'.format('Total import rows:', str(len(data))))
 	db.insert_rows(data, next_available_row(db))
-	client.open('Logistics Data').worksheet('update').update('B2', \
+	client.open('Logistics Data').worksheet('update').update('B2',
 		datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M'))
+
 
 # takes a string for a location of a json file containing the google api credentials
 # returns the gspread authentication variable
 def get_gspread_client(auth_file):
-	scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+	scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 	creds = ServiceAccountCredentials.from_json_keyfile_name(auth_file, scope)
 	return(gspread.authorize(creds))
+
 
 # takes a gspread google sheet variable
 # returns the missing dates between 3/21/2021 (earliest date) and today
@@ -55,18 +58,18 @@ def get_missing_dates(db):
 		courierDates = pd.unique(courier['date'])
 	except KeyError:
 		courierDates = []
-	realDates = pd.date_range(datetime.datetime(2021,3,21), today).strftime('%-m/%-d/%y')
+	realDates = pd.Series(pd.date_range(datetime.datetime(2021, 3, 21), today))
+	realDates = realDates.apply(lambda x: x.strftime("%m/%d/%y").lstrip("0").replace("/0", "/"))
 	diffDates = list(set(realDates)-set(courierDates))
-	diffDates.sort()
 	return(diffDates)
+
 
 # takes a gspread authentication variable and string of a date
 # returns a table containing the number of orders, false trips, lates, by study for the given date
 def get_courier_data(client, date):
 	# connects to the kpi and exceptions in google sheets
-	kpi = client.open('UW Brotman KPIs Excel'+str(datetime.datetime.strftime(date,'%-m_%-d_%Y'))).get_worksheet(0)
-	exceptions = client.open('UW Brotman Exceptions Excel'+ \
-		str(datetime.datetime.strftime(date,'%-m_%-d_%Y'))).get_worksheet(0)
+	kpi = client.open(f'UW Brotman KPIs Excel{date.replace("/","_")}').get_worksheet(0)
+	exceptions = client.open(f'UW Brotman Exceptions Excel{date.replace("/","_")}').get_worksheet(0)
 
 	# create dataframes for both sheets
 	kpiDF = pd.DataFrame(kpi.get_all_records())
@@ -77,8 +80,9 @@ def get_courier_data(client, date):
 		return
 
 	# column names
-	columns = ['OrderNumber','CreateDate','ProjectName','Out/Return','PUZip','DLZip','FalseTrip', 'Late']
-	
+	columns = ['OrderNumber', 'CreateDate', 'ProjectName', 'Out/Return',
+			   'PUZip', 'DLZip', 'FalseTrip', 'Late']
+
 	# The exceptions table does not have PUZip or DLZip but it may be added in the future.
 	# Setting to empty values since columns are needed for the concat
 	try:
@@ -92,26 +96,29 @@ def get_courier_data(client, date):
 
 	table['ParticipantZip'] = table.apply(participant_zip, axis=1)
 
-	table = table.groupby(['CreateDate','ProjectName','Out/Return','ParticipantZip'],
+	table = table.groupby(['CreateDate', 'ProjectName', 'Out/Return', 'ParticipantZip'],
 		).agg({
-			'Out/Return':'count',
-			'FalseTrip':sum,
-			'Late':sum,
+			'Out/Return': 'count',
+			'FalseTrip': sum,
+			'Late': sum,
 		})
 	# reset index does not work with two Out/Return columns so renaming to orders
 	table.columns = ['orders', 'ft', 'late']
 	table.reset_index(inplace=True)
-	print('{: <30}{}'.format(str(date.date())+' Orders:',str(sum(table['orders']))))
+	print('{: <30}{}'.format(str(date.date())+' Orders:', str(sum(table['orders']))))
 	return(table.values.tolist())
+
 
 # finds the participant's zipcode based on an order's Out/Return value
 def participant_zip(x):
 	return x['PUZip'] if x['Out/Return'] == 'Return' else x['DLZip']
 
-#returns the next empty row in a sheet
+
+# returns the next empty row in a sheet
 def next_available_row(worksheet):
 	str_list = list(filter(None, worksheet.col_values(1)))
 	return len(str_list)+1
 
+
 if __name__ == "__main__":
-    main()
+	main()
