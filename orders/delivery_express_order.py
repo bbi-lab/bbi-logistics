ae142b57478ddfdf1061761a1da9739b4843df62
cooperqmarshall <qballer@uw.edu>
2022-02-15 13:51:26 -0800

    AIRS Study added to delivery express ordering. Redcap API key nessesary.

--- orders/delivery_express_order.py
+++ orders/delivery_express_order.py
@@ -1,159 +1,212 @@
 #!/usr/bin/env python3
 
-import os
 import re
-import sys
 import json
-import envdir
-import requests
-import pandas as pd
-from pathlib import Path
+import sys
+from os import environ, path
 from datetime import datetime
 from urllib.parse import urlparse
+from redcap import Project
+import pandas as pd
+import envdir
 
-base_dir = Path(__file__).resolve().parent.parent.resolve()
-envdir.open(base_dir / f'.env/redcap')
+base_dir = path.abspath(__file__ + "/../../")
+envdir.open(path.join(base_dir, '.env/redcap'))
+sys.path.append(base_dir)
 
-# set to run every X minutes
+# pylint: disable=import-error, wrong-import-position
+from etc.redcap_variable_map import project_dict
 
 exportColumns = [
-    'Record Id',
-    'Today Tomorrow',
-    'Order Date',
-    'Project Name',
-    'First Name',
-    'Last Name',
-    'Street Address',
-    'Apt Number',
-    'City',
-    'State',
-    'Zipcode',
-    'Delivery Instructions',
-    'Email',
-    'Phone',
-    'Notification Pref',
-    'Pickup Location']
-replaceAddressColumns = ['Street Address 2','Apt Number 2','City 2','State 2','Zipcode 2']
-coreAddressColumns = ['Street Address','Apt Number','City','State','Zipcode']
-otherReplace = ['First Name','Last Name','Delivery Instructions','Email','Phone','Notification Pref',]
+    'Record Id', 'Today Tomorrow', 'Order Date', 'Project Name', 'First Name',
+    'Last Name', 'Street Address', 'Apt Number', 'City', 'State', 'Zipcode',
+    'Delivery Instructions', 'Email', 'Phone', 'Notification Pref',
+    'Pickup Location'
+]
+
 
 def main():
-    '''Gets orders from redcap and send them to dispatch'''
-    # config file for variable name mapping across projects
-    with open(base_dir / f'etc/redcap_variable_map.json', 'r') as f:
-        projectDict = json.load(f)
-    with open(base_dir / f'etc/zipcode_variable_map.json', 'r') as f:
-        zipcode_var_map = json.load(f)
-    with open(base_dir / f'etc/zipcode_county_map.json', 'r') as f:
-        zipcode_county_map = json.load(f)
-
+    '''Gets orders from redcap and combine them in a csv file'''
     order_export = pd.DataFrame(columns=exportColumns, dtype='string')
 
-    for project in projectDict:
-        sys.stdout.write(f'Kit orders for {project}: ')
-        sys.stdout.flush()
-        # get data from redcap report and format
-        order_data = get_redcap_orders(project, projectDict)
-
-        print(len(order_data.index))
-        if len(order_data.index) < 1:
+    for project in project_dict:  #(p for p in project_dict if p == 'AIRS'):
+        print(f'Kit orders for {project}: ', end='')
+        project_orders = get_redcap_orders(project)
+        orders = len(project_orders.index.get_level_values(0).unique())
+        print(orders)
+        if orders < 1:
             continue
+        project_orders = format_orders(project_orders, project)
+        order_export = pd.concat([order_export, project_orders],
+                                 ignore_index=True)
+    export_orders(order_export)
+    # TODO: import to DE
 
-        # substitutes raw values with lables for zipcode
-        order_data = clean(order_data, project, zipcode_var_map)
 
-        # add a column for the project the order belongs to
-        order_data['Project Name'] = order_data.apply( \
-            lambda row: assign_project(row, project, zipcode_county_map), axis=1).astype('string')
-        
-        # only use columns specified in the the exportColumns array
-        order_data = order_data[order_data.columns.intersection(exportColumns)]
+def format_orders(project_orders, project):
+    project_orders = format_longitudinal(project, project_orders)
+    project_orders = map_scan_zipcodes(project_orders, project)
+    project_orders['Project Name'] = project_orders.apply(
+        lambda row: assign_project(row, project), axis=1)
+    project_orders.index.names = ['Record Id', 'redcap_event_name']
+    project_orders.reset_index(level=0, inplace=True)
+    # only use columns specified in the exportColumns
+    project_orders = project_orders[project_orders.columns.intersection(
+        exportColumns)]
+    return project_orders
 
-        # combine orders in one data frame
-        order_export = pd.concat([order_export, order_data], ignore_index=True)
 
-    print(order_export.groupby(['Project Name']).size().reset_index(name='counts'))
-
-    order_export.to_csv(base_dir / f'data/DeliveryExpressOrder{datetime.now().strftime("%Y_%m_%d_%H_%M")}.csv', \
-        index=False)
-
-    # TODO:
-    # check for bad addresses by sending to DE then handeling bounce backs
-    # import to DE results in an error
-    # errors get routed to PC team email
-
-def get_redcap_orders(project, projectDict):
+def get_redcap_orders(project):
+    '''Fetch content of order reports for a given project'''
     if project == "HCT":
-        url = urlparse(os.environ.get("HCT_REDCAP_API_URL"))
+        url = urlparse(environ.get("HCT_REDCAP_API_URL"))
+    elif project == "AIRS":
+        url = urlparse(environ.get("AIRS_REDCAP_API_URL"))
     else:
-        url = urlparse(os.environ.get("REDCAP_API_URL"))
+        url = urlparse(environ.get("REDCAP_API_URL"))
+    api_key = environ.get(
+        f"REDCAP_API_TOKEN_{url.netloc}_{project_dict[project]['project_id']}")
+    redcap_project = Project(url.geturl(), api_key)
+    order_report = redcap_project.export_reports(
+        report_id=project_dict[project]['Report Id'],
+        format='df').rename(columns=project_dict[project])
+    return order_report
 
-    data = {
-        'token': os.environ.get(f"REDCAP_API_TOKEN_{url.netloc}_{projectDict[project]['project_id']}"),
-        'content': 'report',
-        'report_id': projectDict[project]['Report Id'],
-        'format': 'json',
-        'type': 'flat',
-        'rawOrLabel': 'raw',
-        'rawOrLabelHeaders': 'raw',
-        'exportCheckboxLabel': 'false',
-        'returnFormat': 'json'
-    }
 
-    r = requests.post(url.geturl(),data=data)
-
-    # normalize column names
-    try:
-        orders = pd.DataFrame(r.json(), dtype='string').rename(columns=projectDict[project])
-    except:
-        print(f'unable to create DataFrame with: {r.json()}')
-        return
-
-    # if there are columns that hint at possible replacement addresses
-    if all(val in orders.columns for val in replaceAddressColumns):
-        orders['Order Date'] = pd.to_datetime(orders['Order Date'])
-        orders['Order Date'].replace('', pd.NA, inplace=True)
-
-        # save original addresses
-        original_address = orders.loc[orders['redcap_event_name'] == 'enrollment_arm_1']
-        orders = orders.loc[~(orders['redcap_event_name'] == 'enrollment_arm_1')] \
+def format_longitudinal(project, orders):
+    '''Reduce logitudinal projects to 1 order per row'''
+    if project_dict[project]['project_type'] != 'longitudinal':
+        return orders
+    orders['Order Date'] = pd.to_datetime(orders['Order Date'])
+    orders['Order Date'].replace('', pd.NA, inplace=True)
+    if project == 'HCT':
+        original_address = orders.filter(like='enrollment_arm_1', axis=0)
+        orders = orders.filter(like='encounter_arm_1', axis=0) \
             .dropna(subset=['Order Date']) \
-            .sort_values(by='Order Date', ascending=False) \
-            .drop_duplicates(subset=['Record Id'], keep='first')
+            .query("~index.duplicated(keep='last')") \
+            .apply(lambda row: use_best_address(original_address, row, 'enrollment_arm_1'), axis=1)
 
-        orders = orders.apply(lambda row: use_best_address(original_address, row), axis=1)
+    if project == 'AIRS':
+        order_fields_2 = [
+            "Order Date 2", "Today Tomorrow 2", "Street Address 3",
+            "Apt Number 3", "City 3", "State 3", "Zipcode 3",
+            "Delivery Instructions 2", "Pickup Location 2"
+        ]
+        order_fields = [
+            "Order Date", "Today Tomorrow", "Street Address 2", "Apt Number 2",
+            "City 2", "State 2", "Zipcode 2", "Delivery Instructions",
+            "Pickup Location"
+        ]
+        original_address = orders.filter(like='screening_and_enro_arm_1',
+                                         axis=0)
+        orders = orders.filter(like='week', axis=0) \
+            .dropna(subset=['Order Date', 'Order Date 2'], how='all') \
+            .reset_index(level='redcap_event_name') \
+            .query("~index.duplicated(keep='last')") \
+            .set_index('redcap_event_name', append=True)
 
+        def determine_order(row):
+            if row.loc[order_fields_2].notnull().sum() > 1:
+                order = row.loc[order_fields_2]
+                order.index = order_fields
+            else:
+                order = row.loc[order_fields]
+            return order
+
+        orders.loc[:, order_fields] = orders.apply(determine_order, axis=1)
+        orders = orders.apply(lambda row: use_best_address(
+            original_address, row, 'screening_and_enro_arm_1'),
+                              axis=1)
+
+    elif project == 'Cascadia':
+        original_address = orders[
+            orders['redcap_repeat_instrument'] != 'symptom_survey']
+        orders = orders[orders['redcap_repeat_instrument'] == 'symptom_survey'] \
+            .dropna(subset=['pu_time']) \
+            .apply(lambda row: use_best_address(original_address, row), axis=1)
     return orders
 
-def use_best_address(original_address, row):
-    # if there is not a replacement address use the original address for a specific record_id
-    original = original_address.loc[(original_address['Record Id'] == row['Record Id'])]
-    if row[replaceAddressColumns].replace('',pd.NA).isnull().all():
-        for val in coreAddressColumns:
-            row[val] = original.iloc[0][val]
+
+def use_best_address(original_address, row, event=''):
+    '''if there is not a replacement address use the original address for a
+    specific order Orders are filtered based on record_id and event. A custom
+    event may be specified'''
+    replace_address_columns = [
+        'Street Address 2', 'Apt Number 2', 'City 2', 'State 2', 'Zipcode 2'
+    ]
+    core_address_columns = [
+        'Street Address', 'Apt Number', 'City', 'State', 'Zipcode'
+    ]
+    other_replace = [
+        'First Name', 'Last Name', 'Delivery Instructions', 'Email', 'Phone',
+        'Notification Pref', 'Record Id'
+    ]
+    record_index = row.name if not event else (row.name[0], event)
+    original = original_address.filter(items=[record_index], axis=0)
+
+    if set(replace_address_columns).issubset(
+            row.index) and row[replace_address_columns].notnull().all():
+        update = row.loc[replace_address_columns]
+        for val in core_address_columns:
+            # the replacement fields has a trailing 2
+            row[val] = update.loc[str(val + ' 2')]
     else:
-        update = row.loc[replaceAddressColumns]
-        for val in coreAddressColumns:
-            # the replacement fields have a trailing 2
-            row[val] = update.loc[str(val+' 2')]
-    for val in otherReplace:
-        row[val] = original.iloc[0][val]
+        for val in core_address_columns:
+            row[val] = original.iloc[0][val]
+    for val in other_replace:
+        try:
+            row[val] = original.iloc[0][val]
+        except KeyError:
+            pass
     return row
 
-def clean(orders, project, zipcode_var_map):
-    if re.search('SCAN', project):
-        orders['Zipcode'] = orders.apply(lambda row: zipcode_var_map['SCAN'][row['Zipcode']] if row['Zipcode'] != '' else row['Zipcode'], axis=1)
+
+def map_scan_zipcodes(orders, project):
+    '''maps raw to labeled zipcode values'''
+    if not re.search('SCAN', project):
+        return orders
+    zipcode_var_map = json.load(
+        open(path.join(base_dir, 'etc/zipcode_variable_map.json'),
+             'r',
+             encoding="utf8"))
+    orders['Zipcode'] = orders['Zipcode'].apply(
+        lambda x: zipcode_var_map['SCAN'][str(x)] if x != '' else x)
     return orders
 
-def assign_project(row, project, zipcode_county_map):
-    if project == 'HCT':
-        return 'HCT'
-    elif re.search('SCAN', project):
+
+def assign_project(row, project):
+    '''Returns the DE project name for an order'''
+    zipcode_county_map = json.load(
+        open(path.join(base_dir, 'etc/zipcode_county_map.json'),
+             'r',
+             encoding="utf8"))
+    if re.search('SCAN', project):
         if row['Zipcode'] in zipcode_county_map['SCAN KING']:
             return 'SCAN_KING'
         if row['Zipcode'] in zipcode_county_map['SCAN PIERCE']:
             return 'SCAN_PIERCE'
-    return ''
+    if project == 'Cascadia':
+        if row['City'] == 'SEA':
+            return 'CASCADIA_SEA'
+        if row['City'] == 'PDX':
+            return 'CASCADIA_PDX'
+    if project == "HCT":
+        return "HCT"
+    if project == "AIRS":
+        return "AIRS"
+    print(f'Unknown project for record {row["Record Id"]} in {project}')
+    return project
+
+
+def export_orders(orders):
+    print(orders.groupby(['Project Name']).size() \
+                .reset_index(name='counts'))
+    orders.to_csv(path.join(
+        base_dir,
+        f'data/DeliveryExpressOrder{datetime.now().strftime("%Y_%m_%d_%H_%M")}.csv'
+    ),
+                  index=False)
+
 
 if __name__ == "__main__":
     main()
