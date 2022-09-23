6463fea0e957803eb96bb660e41e0ce657d38d11
Dylan Renard <75507784+drenard921@users.noreply.github.com>
2022-09-23 09:29:28 -0700

    Correct Tableau Shell Script
    This shell scripts runs several of our dashboard updating scripts
    using crontabs. The error preventing the script from running
    had to do with missing filepaths in the pipenv run commands.
    To fix this I've added the complete server file paths to allow
    the script to run.

--- update_dashboards/update.sh
+++ update_dashboards/update.sh
@@ -1,5 +1,9 @@
-
-pipenv run python3 courier.py
-pipenv run python3 kits_shipped.py
-pipenv run python3 pc.py
-pipenv run python3 tpchd.py
+#! /bin/bash
+pipenv run python3 /home/ec2-user/logistics/update_dashboards/courier.py
+echo "Courier Data Extract Successful"
+pipenv run python3 /home/ec2-user/logistics/update_dashboards/kits_shipped.py
+echo "Kits Shipped Data Extract Successful"
+pipenv run python3 /home/ec2-user/logistics/update_dashboards/pc.py
+echo "Participant-Comm Data Extract Sucessful"
+pipenv run python3 /home/ec2-user/logistics/update_dashboards/tpchd.py
+echo "TPCHD Data Extract Successful"
